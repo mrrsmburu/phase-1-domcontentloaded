ba720c938fc646ecc362e5238292e1a9ac021ff8
mrrsmburu <mrrsmburu@gmail.com>
2023-10-23 21:04:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("the DOM has loaded");
+    const text = document.querySelector('p');
+    text.textContent = "This is very cool!"
+
+} )
